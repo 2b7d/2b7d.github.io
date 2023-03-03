263dffdaecb5253795d4ae8d550b52bdbff2e7c9
fosseddy <markfosseddy@gmail.com>
2023-03-03 16:34:08 +0500

    semantics, styling

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const cmd1 = { text: "me --help", index: 0 };
 const cmd2 = { text: "cat my-interests.txt", index: 0 };
 
 const lines = document.querySelectorAll(".terminal-line");
-const sections = document.querySelectorAll("section");
+const cmdData = document.querySelectorAll(".cmd-data");
 const cursor = document.querySelector(".cursor");
 
 function printCmd(cmd, htmlElem) {
@@ -26,16 +26,16 @@ function sleep(time) {
 
 async function main() {
     await sleep(500);
-    await printCmd(cmd1, lines[0].querySelector("#cmd-text"));
+    await printCmd(cmd1, lines[0].querySelector(".ps1__cmd"));
     await sleep(500);
-    sections[0].classList.remove("hidden");
+    cmdData[0].classList.remove("hidden");
 
     lines[1].classList.remove("hidden")
     lines[1].append(cursor);
     await sleep(500);
-    await printCmd(cmd2, lines[1].querySelector("#cmd-text"));
+    await printCmd(cmd2, lines[1].querySelector(".ps1__cmd"));
     await sleep(500);
-    sections[1].classList.remove("hidden");
+    cmdData[1].classList.remove("hidden");
 
     lines[2].classList.remove("hidden");
     lines[2].append(cursor);
