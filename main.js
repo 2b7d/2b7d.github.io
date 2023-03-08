1810a812dc89fac57f60776765504a5ea2806b4d
fosseddy <markfosseddy@gmail.com>
2023-03-08 11:03:52 +0500

    clean up

--- main.js
+++ main.js
@@ -26,20 +26,20 @@ function sleep(time) {
 
 async function main() {
     await sleep(500);
-    await printCmd(cmd1, lines[0].querySelector(".ps1__cmd"));
+    await printCmd(cmd1, lines[0].querySelector(".cmd"));
     await sleep(500);
     cmdData[0].classList.remove("hidden");
 
     lines[1].classList.remove("hidden")
     lines[1].append(cursor);
     await sleep(500);
-    await printCmd(cmd2, lines[1].querySelector(".ps1__cmd"));
+    await printCmd(cmd2, lines[1].querySelector(".cmd"));
     await sleep(500);
     cmdData[1].classList.remove("hidden");
 
     lines[2].classList.remove("hidden");
     lines[2].append(cursor);
-    cursor.classList.add("cursor--blink");
+    cursor.classList.add("blinking");
 }
 
 window.addEventListener("load", () => {
